d6cbbf06119a0038c83a234725a1a4be7dc180a6
tiagoinaba <tiago.inaba@gmail.com>
2023-09-03 20:13:31 -0300

    Update header.tsx

--- app/(dashboard)/produtos/components/header.tsx
+++ app/(dashboard)/produtos/components/header.tsx
@@ -6,6 +6,7 @@ import React from "react";
 
 export const Header = () => {
   const onOpen = useStoreModal((state) => state.onOpen);
+
   return (
     <div className="flex justify-between">
       <h1 className="font-bold text-2xl">Produtos</h1>
